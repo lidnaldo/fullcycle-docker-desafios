903efa1cee2bbe11728dc4c8150d6609ede50873
lidnaldomachado <lidnaldo@gmail.com>
2024-06-05 23:36:17 -0300

    ajustes para retirar diretorios mysql e node_modules

--- desafio_nginx_node/node/index.js
+++ desafio_nginx_node/node/index.js
@@ -1,53 +1,64 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
-console.log('Conexao estabelecida')
 
-const casual = require('casual');
-let nomeAleatorio = casual.full_name;
-console.log(nomeAleatorio);
-const sql = `INSERT INTO people(name) values('${nomeAleatorio}')`
-connection.query(sql)
-console.log('Inserido nome: ' + nomeAleatorio)
-connection.end()
-
-
-app.get('/', (req,res) => {
-    const connection = mysql.createConnection(config)
-    connection.query('SELECT * FROM people', (err, rows) => {
-        if(err) throw err;
-
-        let response = '<h1>Full Cycle Rocks!</h1><ul>';
-        rows.forEach((row) => {
-            response += `<li>ID: ${row.id}, Nome: ${row.name}</li>`;
+function connectWithRetry() {
+    return new Promise((resolve, reject) => {
+        const connection = mysql.createConnection(config);
+        connection.connect((err) => {
+            if (err) {
+                console.error('Erro ao conectar ao banco de dados, tentando novamente em 5 segundos:', err);
+                setTimeout(() => connectWithRetry().then(resolve).catch(reject), 5000);
+            } else {
+                console.log('Conexão estabelecida');
+                resolve(connection);
+            }
         });
-        response += '</ul>';
-
-        res.send(response);
-        connection.end();
-    });   
-})
-
-app.listen(port, () => {
-    console.log('Rodando Node na porta ' + port)
-    
-    const connection = mysql.createConnection(config)
-    connection.query('SELECT * FROM people', (err, rows) => {
-        if(err) throw err;
-
-        console.log('Conteúdo da tabela people:');
-        rows.forEach((row) => {
-            console.log(`ID: ${row.id}, Nome: ${row.name}`);
-        });
-
-        connection.end();
     });
-})
+}
+
+(async () => {
+    const connection = await connectWithRetry();
+
+    const casual = require('casual');
+    let nomeAleatorio = casual.full_name;
+    console.log(nomeAleatorio);
+    const sql = `INSERT INTO people(name) values('${nomeAleatorio}')`;
+    connection.query(sql);
+    console.log('Inserido nome: ' + nomeAleatorio);
+
+    app.get('/', (req, res) => {
+        connection.query('SELECT * FROM people', (err, rows) => {
+            if (err) throw err;
+
+            let response = '<h1>Full Cycle Rocks!</h1><ul>';
+            rows.forEach((row) => {
+                response += `<li>ID: ${row.id}, Nome: ${row.name}</li>`;
+            });
+            response += '</ul>';
+
+            res.send(response);
+        });
+    });
+
+    app.listen(port, () => {
+        console.log('Rodando Node na porta ' + port);
+
+        connection.query('SELECT * FROM people', (err, rows) => {
+            if (err) throw err;
+
+            console.log('Conteúdo da tabela people:');
+            rows.forEach((row) => {
+                console.log(`ID: ${row.id}, Nome: ${row.name}`);
+            });
+        });
+    });
+})();
